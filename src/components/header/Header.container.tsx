d4a6e05fe2a62cf7c21c4232f509ab7a9dee5452
miiin_sseong <wsc03002@naver.com>
2022-06-05 17:00:10 +0900

    finished Header component

---
+++ src/components/header/Header.container.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import HeaderUI from "./Header.presenter";
+
+function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return <HeaderUI isScrolled={isScrolled} />;
+}
+
+export default Header;
