dcd0c6af30b76af3c77636fdbe8ea23c1c7b555f
miiin_sseong <wsc03002@naver.com>
2022-06-08 01:08:11 +0900

    create Modal

---
+++ src/components/units/modal/Modal.tsx
@@ -0,0 +1,105 @@
+import { PlusIcon, XIcon } from "@heroicons/react/outline";
+import MuiModal from "@mui/material/Modal";
+import { useEffect, useState } from "react";
+import { FaPlay } from "react-icons/fa";
+import ReactPlayer from "react-player/lazy";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { modalState, movieState } from "../../../commons/atoms/modalAtom";
+import { Element, Genre } from "../../../commons/types/typing";
+
+function Modal() {
+  const [showModal, setShowModal] = useRecoilState(modalState);
+  const [movie, setMovie] = useRecoilState(movieState);
+  const [trailer, setTrailer] = useState("");
+  const [genres, setGenres] = useState<Genre[]>();
+  const [muted, setMuted] = useState(false);
+
+  useEffect(() => {
+    if (!movie) return;
+    async function fetchMovie() {
+      const data = await fetch(
+        `https://api.themoviedb.org/3/${
+          movie?.media_type === "tv" ? "tv" : "movie"
+        }/${movie?.id}?api_key=${
+          process.env.NEXT_PUBLIC_API_KEY
+        }&language=ko-US&append_to_response=videos`
+      )
+        .then((response) => response.json())
+        .catch((err) => console.log(err.message));
+
+      if (data?.videos) {
+        const index = data.videos.results.findIndex(
+          (element: Element) => element.type === "Trailer"
+        );
+        setTrailer(data.videos?.results[index]?.key);
+      }
+      if (data?.genres) {
+        setGenres(data.genres);
+      }
+    }
+
+    fetchMovie();
+  }, [movie]);
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  console.log(trailer);
+
+  return (
+    <MuiModal
+      open={showModal}
+      onClose={handleClose}
+      className="fixex !top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md scrollbar-hide"
+    >
+      <>
+        <button
+          onClick={handleClose}
+          className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]  "
+        >
+          <XIcon className="h-6 w-6" />
+        </button>
+
+        <div className="relative pt-[56.25%] ">
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${trailer}`}
+            width="100%"
+            height="100%"
+            style={{ position: "absolute", top: "0", left: "0" }}
+            playing
+            muted={muted}
+            light={false}
+            loop={true}
+            config={{
+              file: {
+                tracks: [
+                  {
+                    kind: "subtitles",
+                    src: "subs/subtitles.ko.vtt",
+                    srcLang: "ko",
+                    default: true,
+                  },
+                ],
+              },
+            }}
+          />
+          <div className="absolute bottom-10 flex w-full items-center justify-between px-10 ">
+            <div className="flex space-x-2">
+              <button className="flex items-center gap-x-2 rounded bg-white px-8 text-xl font-bold text-black transition hover:bg-[#e6e6e6] ">
+                <FaPlay className="h-7 w-7 text-black" />
+                Play
+              </button>
+
+              <button>
+                <PlusIcon />
+              </button>
+            </div>
+          </div>
+        </div>
+      </>
+    </MuiModal>
+  );
+}
+
+export default Modal;
